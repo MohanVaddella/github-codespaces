51f9605694e14519213665fe1899f45afbef8731
Mohan Vaddella <84492994+MohanVaddella@users.noreply.github.com>
2023-09-11 02:52:11 +0000

    updated portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Mohan Vaddella",
+  title: "Cloud & Devops Enthusiast",
+  email: "mohanvaddella@gmail.com",
+  gitHub: "MohanVaddella",
+  instagram: "mohanvaddella25",
+  linkedIn: "mohanvaddella",
+  medium: "mohanvaddella",
+  twitter: "mohanvaddella",
+  youTube: "mohanvaddella",
 };
 
 const primaryColor = "#4E567E";
